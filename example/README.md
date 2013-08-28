5001d2ad04c285761f9b1bd4262d98cf580ee9a4
Bills <luizpbills@gmail.com>
2013-08-28 16:48:27 -0300

    0.1.0

---
+++ example/README.md
@@ -0,0 +1,5 @@
+####Use browserify to build the bundle
+
+```
+browserify -r ./game.js > bundle.js
+```
--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 var lite = {};
 
+lite.version = '0.1.0';
 lite.EntityManager = require('./src/entity-manager');
 lite.Component = require('./src/component');
 
